a9738bbdc62d3abc034dbf4cb712ad720e74fa56
Ahad Islam <ahad.islam624@gmail.com>
2020-04-02 11:19:49 -0400

    solved the problems

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,26 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (method, index, array) => {
+    if (method === 'GET') {
+        if (presets[index] != undefined) {
+            return [200, presets[index]]
+        } else {
+            return [404]
+        }
+    } else if (method === 'PUT') {
+        if (presets[index] != undefined) {
+            presets[index] = array
+            return [200, array]
+        } else {
+            return [404]
+        }
+    } else if (method != 'GET' || method != 'PUT') {
+        return [400]
+    } else {
+        return [404]
+    }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,34 @@ let snares
 let hiHats
 let rideCymbals
 
+const drums = {
+    'kicks' : kicks = Array(16).fill(false),
+    'snares' : snares = Array(16).fill(false),
+    'hiHats' : hiHats = Array(16).fill(false),
+    'rideCymbals' : rideCymbals = Array(16).fill(false),
+};
+
+const toggleDrum = (drumName, i) => {
+    if (drums.hasOwnProperty(drumName) && i < drums[drumName].length && i >= 0) {
+        drums[drumName][i] = !drums[drumName][i]
+    }
+};
+
+const clear = name => {
+    if (drums.hasOwnProperty(name)) {
+        drums[name].fill(false)
+    }
+
+}
+
+const invert = drumName => {
+    if (drums.hasOwnProperty(drumName)) {
+        for (let i = 0; i < drums[drumName].length; i++) {
+            drums[drumName][i] = !drums[drumName][i];
+        }
+    }
+}
+
+const getNeighborPads = (x, y, size) => {
+    
+}
